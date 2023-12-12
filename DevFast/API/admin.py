b4d0f1136c60dd34c07ccd7bd7152effc1b56c73
Ox03bb <alibouakaz91@Gmail.com>
2023-12-12 19:23:33 +0100

    admin page tabels Done ..!!!

--- DevFast/API/admin.py
+++ DevFast/API/admin.py
@@ -2,9 +2,35 @@ from django.contrib import admin
 from API.models import *
 # Register your models here.
 
-admin.site.register(Costumer)
-admin.site.register(Progress)
-admin.site.register(Peyment)
-admin.site.register(DataReq)
+
+
+class CostumerAdmin(admin.ModelAdmin):
+   list_display = ("user", "phone","is_delivered", "delivery_date",)
+ 
+admin.site.register(Costumer, CostumerAdmin)
+# =====================================================================
+class ProgressAdmin(admin.ModelAdmin):
+   list_display = ("costumer", "img","date", "description")
+ 
+admin.site.register(Progress,ProgressAdmin)
+# =====================================================================
+class PeymentAdmin(admin.ModelAdmin):
+   list_display = ("costumer", "price","is_paid", "date")
+   
+admin.site.register(Peyment,PeymentAdmin)
+# =====================================================================
+class DataReqAdmin(admin.ModelAdmin):
+   list_display = ("costumer", "date","text", "is_finished")
+   
+admin.site.register(DataReq,DataReqAdmin)
+# =====================================================================
+class DataResAdmin(admin.ModelAdmin):
+   list_display = ("img", "datareq","date")
+   
 admin.site.register(DataRes)
+# =====================================================================
+class NotificationAdmin(admin.ModelAdmin):
+   list_display = ("INPUT_CHOICES", "costumer","type")
+   
+
 admin.site.register(Notification)
