2bb299fbc6547f5299e8fac8e177405a472fea09
B-AJ-Amar <amarbouakaz91@gmail.com>
2023-12-12 17:42:49 +0100

    add admin file

---
+++ DevFast/API/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from API.models import *
+# Register your models here.
+
+admin.site.register(Costumer)
+admin.site.register(Progress)
+admin.site.register(Peyment)
+admin.site.register(DataReq)
+admin.site.register(DataRes)
+admin.site.register(Notification)
