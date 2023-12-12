37a830d9a008e18773f1b61b1fe5fd2faa662f34
= <mohammedaouamri5@gmail.com>
2023-12-12 22:55:08 +0000

    TODO:fix file

--- DevFast/API/views.py
+++ DevFast/API/views.py
@@ -14,6 +14,7 @@ from rest_framework.authentication import SessionAuthentication,BasicAuthenticat
 from rest_framework.decorators import authentication_classes,permission_classes
 
 class GetTheProgress(APIView):
+    # ? tsawer ta3 dare bah sayed yefrah b darou 
     authentication_classes = [TokenAuthentication,SessionAuthentication]
     permission_classes = [IsAuthenticated] 
     def get(self, request ):
@@ -32,21 +33,62 @@ class GetTheProgress(APIView):
             return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
+
+class GetTheReq(APIView): 
+    # ? hna neb3atlou wach nahtag (aka rewy ta3 lwra9i)
+    authentication_classes = [TokenAuthentication,SessionAuthentication]
+    permission_classes = [IsAuthenticated] 
+    def get(self, request ):
+        if request.user.is_authenticated:
+            try:
+                costumer_id = Costumer.objects.get(user=request.user).id
+            except Exception as e:
+                print("ProgressView error : ",e)
+                return Response({'error': "not found"},status=404)
+                
+            objects = DataReq.objects.filter(costumer=costumer_id , is_paid=False)
+            serializer = DataReqSerializer(objects, many=True)
+            serialized_data = serializer.data
+            return Response({'date': serialized_data})
+        else:
+            return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
 class SendFile(APIView):
+    # ?  hna hna njim nahtaj (aka rewy ta3 lwra9i)
     authentication_classes = [TokenAuthentication,SessionAuthentication]
     permission_classes = [IsAuthenticated] 
     def post(self, request ):
         if request.user.is_authenticated:
             serializer = DataResSerializer(data=request.data)
             if serializer.is_valid():
+                # DataReq.objects.filter(datareq=serializer.datareq) = True ;
                 serializer.save()
+
                 return Response(status=status.HTTP_200_OK)
             else:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
 
-
+class GetPymentDate(APIView): 
+    # ?  hna neb3atlou bah y5alesni 🤑 
+    authentication_classes = [TokenAuthentication,SessionAuthentication]
+    permission_classes = [IsAuthenticated] 
+    def get(self, request ):
+        if request.user.is_authenticated:
+            try:
+                costumer_id = Costumer.objects.get(user=request.user).id
+            except Exception as e:
+                print("ProgressView error : ",e)
+                return Response({'error': "not found"},status=404)
+                
+            objects = Peyment.objects.filter(costumer=costumer_id ,is_paid=False)
+            serializer = PeymentSerializer(objects, many=True)
+            serialized_data = serializer.data
+            return Response({'data': serialized_data})
+        else:
+            return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
 
  
 class hello(APIView):
