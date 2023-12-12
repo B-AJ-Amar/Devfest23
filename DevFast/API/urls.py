95efa5e9153cb8ba0f02267b889e08b72b9829e4
Ox03bb <alibouakaz91@Gmail.com>
2023-12-12 17:19:33 +0100

    add /prg pointe rsep json

--- DevFast/API/urls.py
+++ DevFast/API/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include   
 from .views import hello 
+from . import views
+
 urlpatterns = [
     path('', hello.as_view()),
+    include('prg/',views.prg,name='prg')
 ]
--- DevFast/API/views.py
+++ DevFast/API/views.py
@@ -3,8 +3,20 @@ from django.http import JsonResponse
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import os
 
 
 class hello(APIView):
     def get(self,request):
         return JsonResponse({"message":"Hello World"})
+    
+
+def prg(request):
+    data = {
+    'date': '2023/12/03',
+    'img': '/media/pro1.jpg' ,
+    'desc': 'decs desc dddqsdqkfjhqfkjhs',
+    }
+
+    response = JsonResponse(data, status=200)
+    return response
