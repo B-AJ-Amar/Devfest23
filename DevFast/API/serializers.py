be211c5aa066b745554e01a0517f8e401b0a272d
= <mohammedaouamri5@gmail.com>
2023-12-12 23:40:27 +0000

    fix serializer

--- DevFast/API/serializers.py
+++ DevFast/API/serializers.py
@@ -1,7 +1,6 @@
 from .models import  Costumer , Progress  , Peyment , DataReq ,  Notification , DataRes
 from rest_framework import serializers
-from backend.settings import base_url
-
+ 
 
 class CostumerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,38 +10,31 @@ class CostumerSerializer(serializers.ModelSerializer):
 
 class ProgressSerializer(serializers.ModelSerializer):
     date = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
-    url = serializers.SerializerMethodField()
     class Meta:
         model = Progress 
         fields = "__all__"
-        
-    def get_url(self, obj):
-        # Assuming your Django app is running on the same machine on port 8000
-        # Adjust the URL accordingly based on your setup
-        
-        return f"{base_url}/media/{obj.url}" if obj.url else None
 
 class PeymentSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     class Meta:
         model = Peyment 
         fields = "__all__"
 
-class PeymentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Peyment 
-        fields = "__all__"
 
 class DataReqSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     class Meta: 
         model = DataReq 
         fields = "__all__"
 
 class DataResSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     class Meta: 
         model = DataRes 
         fields = "__all__"
 
 class NotificationSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     class Meta: 
         model = Notification 
         fields = "__all__"
