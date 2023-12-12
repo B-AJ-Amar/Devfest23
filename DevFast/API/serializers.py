309a580e897c65d361b98c7393dae52b837e31a6
= <mohammedaouamri5@gmail.com>
2023-12-12 16:54:07 +0000

    serializers

---
+++ DevFast/API/serializers.py
@@ -0,0 +1,43 @@
+from .models import Profile , Costumer , Progress  , Peyment , DataReq ,  Notification , DataRes
+from rest_framework import serializers
+ 
+
+class CostumerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Costumer
+        fields = "__all__"
+
+
+class ProgressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Progress 
+        fields = "__all__"
+
+
+class PeymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Peyment 
+        fields = "__all__"
+
+class PeymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Peyment 
+        fields = "__all__"
+
+class DataReqSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = DataReq 
+        fields = "__all__"
+
+
+class DataResSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = DataRes 
+        fields = "__all__"
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Notification 
+        fields = "__all__"
+
+ 
