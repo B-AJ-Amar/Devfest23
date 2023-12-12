faa6ff81dbdd23fb93c181f98e415af422d450be
= <mohammedaouamri5@gmail.com>
2023-12-12 17:47:58 +0000

    APIView-img

--- DevFast/API/urls.py
+++ DevFast/API/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include   
-from .views import hello 
+from .views import hello  , GetTheProgress
 urlpatterns = [
     path('', hello.as_view()),
+    path('WOW', GetTheProgress.as_view()),
 ]
--- DevFast/API/views.py
+++ DevFast/API/views.py
@@ -3,8 +3,24 @@ from django.http import JsonResponse
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import Progress
+from .serializers import ProgressSerializer
+from rest_framework import status
+
+class GetTheProgress(APIView):
+    def get(self, request ):
+        if request.user.is_authenticated:
+            objects = Progress.objects.filter(customer=request.user.customer)
+            serializer = ProgressSerializer(objects, many=True)
+            serialized_data = serializer.data
+            return Response({'progress': serialized_data})
+        else:
+            return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class hello(APIView):
     def get(self,request):
         return JsonResponse({"message":"Hello World"})
+    
+# class Send
