95dee0e5fdb5d9f9edc1de0c75ec22b153901a02
B-AJ-Amar <amarbouakaz91@gmail.com>
2023-12-13 05:16:39 +0100

    fix admin 3

--- DevFast/backend/urls.py
+++ DevFast/backend/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings #*
 from django.conf.urls.static import static #*
 urlpatterns = [
     path('admin/', include('admin_material.urls')),
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('API.urls')),
     # path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
